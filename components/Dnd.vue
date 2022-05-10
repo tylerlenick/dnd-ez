422428ba3ff1b26f13dc42b8296058d841dd4efd
tyler <tylerlenick@gmail.com>
2022-05-10 19:16:27 -0400

    hey api working

--- components/Dnd.vue
+++ components/Dnd.vue
@@ -12,17 +12,19 @@
             </div>
             <input v-model="queryString" type="search" id="default-search"
                 class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search Mockups, Logos..." required="">
-            <button @click="pushData" type="submit"
-                class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                placeholder="Search Conditions, Spells..." required="">
         </div>
+        <pre>{{ res.results }}</pre>
     </form>
-    <div v-html="data"></div>
 </template>
 
 <script setup>
-import { ref } from "vue"
-const { queryString } = ref()
-const { data } = await useFetch(() => `https://www.dnd5eapi.co/api/${queryString.value}` )
+//import { ref } from "vue"
+let queryString = ref('')
+let { data: res } = await useFetch(() => `https://www.dnd5eapi.co/api/${queryString.value}`, { pick: ['results'] } )
+
+
 
 </script>
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto bg-gray-300 p-8">
-    <h1 class="font-bold text-gray-600 text-lg">Hello, Tailwind 3!</h1>
+    <h1 class="font-bold text-gray-600 text-lg">Hello, Dnd EZ!</h1>
   </div>
   <Dnd/>
 </template>
